ddf142c53051dda482c9aa99f0cd42f9a8590a7a
zxydeliaputri <zxydliaaptrii@gmail.com>
2022-09-03 13:33:05 +0700

    mindahin url & key

--- utils/supabaseClient.js
+++ utils/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://cwwhqqvhqkkmdnheqkdw.supabase.co/";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN3d2hxcXZocWtrbWRuaGVxa2R3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjIxODA0MzUsImV4cCI6MTk3Nzc1NjQzNX0.9gZQedl_67eWuxsPeUFMGdSgM6T9DkoGyLU5dDeuF6E";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
