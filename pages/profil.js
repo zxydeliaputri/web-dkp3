f44b9c0a25be0e6b360f129115c6954c138413cc
zxydeliaputri <zxydliaaptrii@gmail.com>
2022-09-24 13:01:39 +0700

    update : bootstrap icon dan halaman profil

--- pages/profil.js
+++ pages/profil.js
@@ -9,7 +9,7 @@ export default function Profil() {
             <div className="card-header bg-dark text-white">
               <h3>Sekilas DKP3</h3>
             </div>
-            <div className="card-body bg-secondary text-dark">
+            <div className="card-body bg-secondary text-white">
               <p>Dinas Ketahanan Pangan, Pertanian dan Perikanan mempunyai tugas pokok membantu wali kota melaksanakan urusan pemerintahan daerah dan tugas pembantuan di bidang pangan, pertanian dan sebagian urusan kelautan dan perikanan</p>
             </div>
           </div>
@@ -18,12 +18,12 @@ export default function Profil() {
             <div className="card-header bg-dark text-white">
               <h3>VISI, MISI, dan MOTTO</h3>
             </div>
-            <div className="card-body bg-primary">
-              <div className="col-md-3 pt-3 pb-3 text-center text-dark bg-success">
+            <div className="card-body bg-info text-white">
+              <div className="col-md-3 pt-3 pb-3 text-center bg-success">
                 <h4>VISI</h4>
               </div>
               <p>" Terwujudnya pelayanan sektor pangan, pertanian dan perikanan yang profesional "</p>
-              <div className="col-md-3 pt-3 pb-3 text-center text-dark bg-success">
+              <div className="col-md-3 pt-3 pb-3 text-center bg-success">
                 <h4>MISI</h4>
               </div>
               <p>
@@ -33,7 +33,7 @@ export default function Profil() {
                   <li>Menciptakan inovasi pelayanan</li>
                 </ol>
               </p>
-              <div className="col-md-3 pt-3 pb-3 text-center text-dark bg-success">
+              <div className="col-md-3 pt-3 pb-3 text-center bg-success">
                 <h4>MOTTO</h4>
               </div>
               <p>Cepat, Tepat, dan Profesional</p>
@@ -42,11 +42,40 @@ export default function Profil() {
 
           <div className="card bg-dark text-white">
             <h3>Kontak</h3>
-            <p>
-              <ol>
-                <li></li>
-              </ol>
-            </p>
+            <div className="row">
+              <div className="col-md-6">
+                <ul>
+                  <li>
+                    <i className="bi bi-globe"></i> https://dkp3.tasikmalayakota.go.id/
+                  </li>
+                  <li>
+                    <i className="bi bi-pin-map"></i> Jl. Leuwidahu No.85, Parakannyasag, Kec. Indihiang, Kota Tasikmalaya, Jawa Barat 46151
+                  </li>
+                  <li>
+                    <i className="bi bi-telephone"></i> (0265) 313024
+                  </li>
+                  <li>
+                    <i className=" bi bi-envelope"></i> dkpp.tasikmalayakota@gamil.com
+                  </li>
+                </ul>
+              </div>
+              <div className="col">
+                <ul>
+                  <li>
+                    <i className="bi bi-instagram"></i> @dkp3.tasikmalayakota
+                  </li>
+                  <li>
+                    <i className="bi bi-twitter"></i> @dkp3tasikkota
+                  </li>
+                  <li>
+                    <i className="bi bi-facebook"></i> Dinas Ketahanan Pangan, Pertanian dan Perikanan Kota Tasikmalaya
+                  </li>
+                  <li>
+                    <i className="bi bi-youtube"></i> DKP3 Kota Tasikmalaya
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
       </div>
