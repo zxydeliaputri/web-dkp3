02b39f4de65b4733d5821c2a49bc11da26961aa5
hilmizul <netspytux@gmail.com>
2022-09-24 12:18:32 +0700

    update layout, components and config images domains

--- pages/informasi.js
+++ pages/informasi.js
@@ -1,12 +1,47 @@
-import Nav from "../components/Nav";
+import Image from "next/image";
+import { useState, useEffect } from "react";
+import { supabase } from '../utils/supabaseClient'
 
-function Katalog() {
+function Informasi() {
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    getItem()
+  }, [])
+
+
+  const getItem = async () => {
+    const { data, error } = await supabase
+      .from("Produk")
+      .select()
+    if (data) {
+      setItems(data)
+    }
+  }
   return (
-    <div>
-      <Nav />
-      <h1>Informasi.....</h1>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12">
+          <div className="row">
+            {items.map((item,i) => {
+              return (
+                <div key={i} className="col-md-3">
+                  <div className="card mt-3 mb-3">
+                    <div className="card-header p-0">
+                      <Image src={item.foto} className="img-thumbnail" width="100" height="100" layout="responsive" alt="" priority />
+                    </div>
+                    <div className="card-body">
+                      <h5>{item.nama}</h5>
+                    </div>
+                  </div>
+                </div>
+              )
+            })}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default Katalog;
+export default Informasi;
--- pages/profil.js
+++ pages/profil.js
@@ -1,10 +1,19 @@
-import Nav from "../components/Nav";
-
-export default function About() {
+export default function Profil() {
   return (
-    <div>
-      <Nav/>
-      <h1>Profil</h1>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12">
+          <div className="card">
+            <div className="card-header">Visi Misi Motto</div>
+            <div className="card-body">
+              <div className="col-md-3 pt-3 pb-3 text-center text-white bg-success">
+                <h4>Visi</h4>
+              </div>
+              <p>visiadladlasdlahld;fa;lsjdflaksjdfl;d</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
