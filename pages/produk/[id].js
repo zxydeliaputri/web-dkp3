c6d43abd6986a432e1e0db2eacddc892482bc881
Delia Putri <112232044+zxydeliaputri@users.noreply.github.com>
2022-09-26 14:23:25 +0700

    memperbaiki image

--- pages/produk/[id].js
+++ pages/produk/[id].js
@@ -6,17 +6,17 @@ import Image from "next/image";
 function DetilProduk() {
   const router = useRouter()
   const { id } = router.query
-  const [item, setItem] = useState([])
+  const [item, setItem] = useState()
 
   useEffect(() => {
     ambilData()
-  },[id])
+  }, [id])
 
   const ambilData = async () => {
     const { data, error } = await supabase
-        .from("Produk")
-        .select()
-        .eq('id', id)
+      .from("Produk")
+      .select()
+      .eq('id', id)
 
     if (data) setItem(data[0])
   }
@@ -25,13 +25,13 @@ function DetilProduk() {
     <div className="container">
       <div className="row mt-3 mb-3">
         <div className="col-md-3">
-            {item ? (
-                <Image src={item.foto} width={100} height={100} layout="responsive" alt="" priority />
-            ) : 'sedang memuat...'}
+          {item ? (
+            <Image src={item.foto} width={100} height={100} layout="responsive" alt="foto" priority />
+          ) : 'sedang memuat...'}
         </div>
         <div className="col">
-            <h1>{item ? item.nama : 'sedang memuat...'}</h1>
-            {item ? item.deskripsi : 'sedang memuat...'}
+          <h1>{item ? item.nama : 'sedang memuat...'}</h1>
+          {item ? item.deskripsi : 'sedang memuat...'}
         </div>
       </div>
     </div>
